011a30c9f348f1a4f4824274f1df9332ac31a22e
İsmail Çakır <ismailcakirnet@gmail.com>
2022-07-29 12:41:42 +0300

    Added themes file

---
+++ core/themes/text_theme.dart
@@ -0,0 +1,5 @@
+class AppThemeTextTheme {
+  // add a private constructor to prevent this class being instantiated
+  // e.g. invoke `AppConfig()` accidentally
+  AppThemeTextTheme._();
+}
---
+++ core/themes/themes.dart
@@ -0,0 +1,5 @@
+class AppTheme {
+  // add a private constructor to prevent this class being instantiated
+  // e.g. invoke `AppConfig()` accidentally
+  AppTheme._();
+}
