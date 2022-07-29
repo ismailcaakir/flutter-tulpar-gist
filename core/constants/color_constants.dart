f38f0a4f4902ace0a8784a0a9691ae4bc452d96d
İsmail Çakır <ismailcakirnet@gmail.com>
2022-07-29 16:16:12 +0300

    Added constants files

---
+++ core/constants/color_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColorConstants {
+  // add a private constructor to prevent this class being instantiated
+  // e.g. invoke `AppConfig()` accidentally
+  AppColorConstants._();
+
+  // the properties are static so that we can use them without a class instance
+  // e.g. can be retrieved by `AppConfig.appName`.
+  static const Colors white = Colors.white;
+}
---
+++ core/constants/string_constants.dart
@@ -0,0 +1,9 @@
+class AppStringConstants {
+  // add a private constructor to prevent this class being instantiated
+  // e.g. invoke `AppConfig()` accidentally
+  AppStringConstants._();
+
+  // the properties are static so that we can use them without a class instance
+  // e.g. can be retrieved by `AppConfig.appName`.
+  static const String example = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+}
